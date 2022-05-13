5106c299f6d2638c53a1df77ae8a71a1c3dc461d
Krodo15 <artur.jedut@student.pcez.pl>
2022-05-13 17:29:19 +0200

    nadal dużo kodu ale poprawione kółko i krzyżyk

--- app.js
+++ app.js
@@ -19,18 +19,7 @@ function clickField(e){
 for(x=1; x<10 ; x++){
     p[x] = document.getElementById("p"+x).innerHTML;
 }
-
-
-//    p1 = document.getElementById("p1").innerHTML;
-//    p2 = document.getElementById("p2").innerHTML;
-//    p3 = document.getElementById("p3").innerHTML;
-//    p4 = document.getElementById("p4").innerHTML;
-//    p5 = document.getElementById("p5").innerHTML;
-//    p6 = document.getElementById("p6").innerHTML;
-//    p7 = document.getElementById("p7").innerHTML;
-//    p8 = document.getElementById("p8").innerHTML;
-//    p9 = document.getElementById("p9").innerHTML;
-   
+  
    win1 = p[1]+p[2]+p[3];
    win2 = p[4]+p[5]+p[6];
    win3 = p[7]+p[8]+p[9];
@@ -48,58 +37,66 @@ for(x=1; x<10 ; x++){
         for(x=1 ; x<4 ; x++){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win2 == "xxx"){
         for(x=4 ; x<7 ; x++){
             console.log("x: "+x);
             document.getElementById("p"+x).innerHTML = '<div style="color:lime";>x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px";>WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px";><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win3 == "xxx"){
         for(x=7 ; x<10 ; x++){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win4 == "xxx"){
         for(x=1 ; x<8 ; x+= 3){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win5 == "xxx"){
         for(x=2 ; x<9 ; x+= 3){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win6 == "xxx"){
         for(x=3 ; x<10 ; x+= 3){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win7 == "xxx"){
         for(x=3 ; x<8 ; x+= 2){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win8 == "xxx"){
         for(x=1 ; x<10 ; x+= 4){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">x</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KRZYŻYKI</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KRZYŻYKI!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
 
 
@@ -110,81 +107,85 @@ for(x=1; x<10 ; x++){
         for(x=1 ; x<4 ; x++){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime";>o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px";>WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px";><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win2 == "ooo"){
         for(x=4 ; x<7 ; x++){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime";>o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win3 == "ooo"){
         for(x=7 ; x<10 ; x++){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win4 == "ooo"){
         for(x=1 ; x<8 ; x+= 3){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win5 == "ooo"){
         for(x=2 ; x<9 ; x+= 3){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win6 == "ooo"){
         for(x=3 ; x<10 ; x+= 3){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win7 == "ooo"){
         for(x=3 ; x<8 ; x+= 2){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px;"><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
     if(win8 == "ooo"){
         for(x=1 ; x<10 ; x+= 4){
             document.getElementById("p"+x).innerHTML = '<div style="color:lime;">o</div>' ;
         }
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">WYGRAŁY KÓŁKA</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px";><h1>WYGRAŁY KÓŁKA!!!</h1></div>' ;
         table.removeEventListener("click",clickField) ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
 
     if(remis == 9){
-        document.getElementById("info").innerHTML = '<div style="font-size:70px;">REMIS</div>' ;
+        document.getElementById("info").innerHTML = '<div style="font-size:70px";><h1>REMIS!!!</h1></div>' ;
         remis = 0 ;
+        document.getElementById("resetButton").className = "visible resetButton";
     }
 }
 
 resetButton.addEventListener("click",reset) ;
 function reset(e){
-    
-    document.getElementById("p1").innerHTML = "" ;
-    document.getElementById("p2").innerHTML = "" ;
-    document.getElementById("p3").innerHTML = "" ;
-    document.getElementById("p4").innerHTML = "" ;
-    document.getElementById("p5").innerHTML = "" ;
-    document.getElementById("p6").innerHTML = "" ;
-    document.getElementById("p7").innerHTML = "" ;
-    document.getElementById("p8").innerHTML = "" ;
-    document.getElementById("p9").innerHTML = "" ;
+     for(x=1 ;x< 10 ; x++ ){
+        p[x] = document.getElementById("p"+x).innerHTML = "";
+     }
     document.getElementById("info").innerHTML = "";
-    table.addEventListener("click",clickField) ;
-    remis = 0 ;
-    startPoint = "x" ;
+    document.getElementById("resetButton").className = "hide resetButton ";
+    table.addEventListener("click",clickField);
+    startPoint = "x";
+    remis = 0;
+
 }
 
 
