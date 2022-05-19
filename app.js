898baa9928788061ee82d43b6f924d0118944361
Krodo15 <artur.jedut@student.pcez.pl>
2022-05-19 17:38:23 +0200

    start button

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
 var pierwiastki = [] ;
 var y = 0 ;
 container = document.getElementsByClassName("symbol");
-//console.log(container.length);
 
-for(x = 0; x < container.length; x++){
-  if(container[x].textContent != "DE" 
-  && container[x].textContent != "DEL" 
-  && container[x].textContent != "57-71" 
-  && container[x].textContent != "89-103"  ){
-      pierwiastki[y] = container[x].textContent ;
-      y++ ;
-  }
-}
-for(x = 0; x < pierwiastki.length; x++){
-//console.log("x:"+x+","+pierwiastki[x]);
-}
-
-random = Math.floor(Math.random() * pierwiastki.length) ;
-console.log(pierwiastki[random]);
-
-for(x = 0 ; x < container.length ; x++){
-  if(container[x].textContent == pierwiastki[random]){
-     wylosowany = container[x] ;
+function randomElements(){
+  for(x = 0; x < container.length; x++){
+    if(container[x].textContent != "DE" 
+    && container[x].textContent != "DEL" 
+    && container[x].textContent != "57-71" 
+    && container[x].textContent != "89-103"  ){
+        pierwiastki[y] = container[x].textContent ;
+        y++ ;
     }
-}
+  }
+  for(x = 0; x < pierwiastki.length; x++){
+  }
+  
+  random = Math.floor(Math.random() * pierwiastki.length) ;
+  console.log(pierwiastki[random]);
+  
+  for(x = 0 ; x < container.length ; x++){
+    if(container[x].textContent == pierwiastki[random]){
+       wylosowany = container[x] ;
+      }
+  }
+
+
 
 console.log(wylosowany);
 wylosowany = wylosowany.parentNode ;
-console.log(wylosowany);
 wylosowany.setAttribute("class","checked");
+}
+
+const easy = document.getElementById("easy");
+const medium = document.getElementById("medium");
+const hard = document.getElementById("hard");
+
+easy.addEventListener('click',gameStart);
+medium.addEventListener('click',gameStart);
+hard.addEventListener('click',gameStart);
 
 
+function gameStart(e){
+  console.log(e.target.id);
+  randomElements();
+}
